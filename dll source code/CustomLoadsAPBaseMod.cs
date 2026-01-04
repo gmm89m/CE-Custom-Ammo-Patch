fe43b0e2a7e3cb1b83baf488f31317db3c837e66
gmm89m <150973951+gmm89m@users.noreply.github.com>
2026-01-04 14:57:46 +0300

    Add files via upload

---
+++ dll source code/CustomLoadsAPBaseMod.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace CustomLoadsAPBase
+{
+    public class CustomLoadsAPBaseMod : Mod
+    {
+        public CustomLoadsAPBaseMod(ModContentPack content)
+            : base(content)
+        {
+            Log.Warning("[CustomLoadsAPBase] Mod constructor");
+
+            var harmony = new Harmony("gmm89m.customloads.apbase");
+            harmony.PatchAll();
+
+            Log.Warning("[CustomLoadsAPBase] Harmony pwned (patched)");
+        }
+    }
+}
---
+++ dll source code/CustomLoadsAPpatchMain.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using CombatExtended;
+using Verse;
+using System.Linq;
+
+namespace CustomLoadsAPBase
+{
+    [HarmonyPatch(typeof(CustomLoads.Bullet.CustomLoad))]
+    [HarmonyPatch(nameof(CustomLoads.Bullet.CustomLoad.GenerateDefs))]
+    public static class Patch_CustomLoad_GenerateDefs
+    {
+        static void Prefix(
+            ref AmmoDef ammoTemplate,
+            ref ThingDef bulletTemplate,
+            bool register,
+            CustomLoads.Bullet.CustomLoad __instance)
+        {
+            Log.Warning("[CustomLoadsAPBase] Patch applied... should be");
+
+            if (ammoTemplate?.ammoClass?.defName != "FullMetalJacket")
+                return;
+
+            var ammoSet = ammoTemplate.AmmoSetDefs?.FirstOrDefault();
+            if (ammoSet == null)
+                return;
+
+            var ap = ammoSet.ammoTypes
+                .Select(x => x.ammo)
+                .FirstOrDefault(a => a.ammoClass?.defName == "ArmorPiercing");
+
+            if (ap == null)
+                return;
+
+            ammoTemplate = ap;
+            bulletTemplate = ap.cookOffProjectile;
+
+            Log.Warning($"[CustomLoadsAPBase] FMJ → AP ({ap.defName})");
+        }
+    }
+}
+
+
